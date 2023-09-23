bc2e28fc51641e1b981702bda14355d0a41c0834
Sankalp Singh <vishal.kotak@mahitechllc.com>
2023-09-23 17:26:21 +0530

    FindTheMissingNumberInArray

---
+++ DSA/src/Arrays/FindTheMissingNumberInArray.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class FindTheMissingNumberInArray {
+
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5,7,8,9,10};
+
+        int maxNumber = 10;
+
+        System.out.println("Missing Number: "+findMissingNumber(array,maxNumber));
+    }
+
+    public static int findMissingNumber(int[] array , int n )
+    {
+        int sumOfNnumbers = (n*(n+1))/2;
+
+        int sumArray = 0;
+
+        for (int element: array)
+        {
+            sumArray = sumArray + element;
+        }
+
+        return sumOfNnumbers - sumArray;
+    }
+}
